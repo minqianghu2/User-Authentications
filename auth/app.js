5fc2defdab290519c2e78e66b9fe0686c6289103
Minqiang Hu <minqianghu@oakland.edu>
2018-09-24 00:55:54 +0000

    bug fix

---
+++ auth/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+//     mongoose = require("mongoose"),
+//     passport = require("passport"),
+//     bodyparser = require("body-parser"),
+//     LocalStrategy = require("passport-local"),
+//     passportLocalMongoose = require("passport-local-mongoose")
+
+// mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app = express();
+
+app.set('view engine','ejs');
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.get("/secret", function(req,res){
+    res.render("secret");
+})
+
+
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("server statred....");
+})
